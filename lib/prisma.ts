383e905ab0508673bd0ab47882d20860b8224991
MUHANGUZI ROLLAND <muhanguzirollands@gmail.com>
2026-03-06 13:34:43 +0200

    NEWONS

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const prismaClientSingleton = () => {
-    return new PrismaClient()
+    const libsql = createClient({
+        url: process.env.DATABASE_URL!,
+        authToken: process.env.DATABASE_AUTH_TOKEN,
+    })
+    const adapter = new PrismaLibSQL(libsql)
+    return new PrismaClient({ adapter })
 }
 
 declare const globalThis: {
